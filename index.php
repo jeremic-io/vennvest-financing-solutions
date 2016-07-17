53641b3f5ab4b76a9cfc587bfdd4a4c375a65d41
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-07-17 16:41:21 +0200

    Add navbar

--- index.php
+++ index.php
@@ -7,6 +7,39 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+  <!-- Start of Main Header Section -->
+  <header class="main-header">
+    <nav class="navbar navbar-inverse">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse-menu" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">
+            <img src="images/logo.jpg" alt="logo">
+            <span>
+              VennVest <br>
+              Financing Solutions
+            </span>
+          </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="collapse-menu">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><i class="fa fa-home"></i> Home</a></li>
+            <li><a href="#about-us"><i class="fa fa-users"></i> About Us</a></li>
+            <li><a href="#finance-solutions"><i class="fa fa-usd"></i> Finance Solutions</a></li>
+            <li><a href="#contact-us"><i class="fa fa-envelope"></i> Contact Us</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+  </header>
 
 
 <!-- Javascript -->
