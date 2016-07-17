e866a21279d10a47167fedffde41150d7d925e8e
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-07-17 16:16:21 +0200

    Add jQuery and Bootstrap

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
   <meta charset="utf-8">
   <meta name=viewport content="width=device-width, initial-scale=1">
   <title>Vennvest Financing Solutions</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
 
+<!-- Javascript -->
+<script src="js/javascript.js"></script>
 </body>
 </html>
